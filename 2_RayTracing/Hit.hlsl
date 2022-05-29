4d173a3e76b861d5b07e17af84bebd6f4bcb4e5e
Christina Obereigner <chrisi.obereigner@gmail.com>
2022-05-29 11:04:58 +0200

    Add shadows to cube

--- 2_RayTracing/Hit.hlsl
+++ 2_RayTracing/Hit.hlsl
@@ -5,13 +5,6 @@ struct STriVertex {
     float4 color;
 };
 
-cbuffer Colors : register(b0)
-{
-    float3 A;
-    float3 B;
-    float3 C;
-}
-
 StructuredBuffer<STriVertex> BTriVertex : register(t0);
 
 // Raytracing acceleration structure, accessed as a SRV
@@ -19,11 +12,32 @@ RaytracingAccelerationStructure SceneBVH : register(t2);
 
 [shader("closesthit")] 
 void ClosestHit(inout HitInfo payload, Attributes attrib) {
-    float3 barycentrics =
-        float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
 
-    // #DXR Extra: Per-Instance Data
-    float3 hitColor = B* barycentrics.x + B*barycentrics.y + B*barycentrics.z;
+	float3 lightPos = float3(-2, 2, 2);
+
+    // Find the world - space hit position
+    float3 worldOrigin = WorldRayOrigin() + RayTCurrent() * WorldRayDirection();
+
+    float3 lightDir = normalize(lightPos - worldOrigin);
+
+    // Fire a shadow ray. The direction is hard-coded here, but can be fetched
+    // from a constant-buffer
+    RayDesc ray;
+    ray.Origin = worldOrigin;
+    ray.Direction = lightDir;
+    ray.TMin = 0.01;
+    ray.TMax = 100000;
+
+    // Initialize the ray payload
+    ShadowHitInfo shadowPayload;
+    shadowPayload.isHit = false;
+	
+	// Trace the ray
+	TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF,	1, 0, 1, ray, shadowPayload);
+	
+	float factor = shadowPayload.isHit ? 0.3 : 1.0;
+
+    float3 hitColor = float3(float3(1.0, 0.0, 0.5) * factor);
 
     payload.colorAndDistance = float4(hitColor, RayTCurrent());
 }
@@ -55,6 +69,6 @@ void PlaneClosestHit(inout HitInfo payload, Attributes attrib)
 	
 	float factor = shadowPayload.isHit ? 0.3 : 1.0;
 
-    float4 hitColor = float4(float3(0.7, 0.7, 0.3) * factor, RayTCurrent());
+    float4 hitColor = float4(float3(0.0, 0.7, 0.3) * factor, RayTCurrent());
     payload.colorAndDistance = float4(hitColor);
 }
